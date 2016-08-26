f3c654f75925d54205abf1a7911ba3f8f401b1a4
Slimikus <slimikus@gmail.com>
2016-08-26 15:58:21 +0300

    создан новый класс

---
+++ SwiftLessonsApp/SwiftLessonsApp/AboutController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutController.swift
+//  SwiftLessonsApp
+//
+//  Created by Admin on 26.08.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+import UIKit
+
+class AboutController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
